3bc7b87045d474ac56dba2fa08179cce4146ede6
Richard Flores <r.flores.l@gmail.com>
2021-10-08 10:12:13 -0300

    [MOD] Estados en vista facturas

--- erps21_reports/__manifest__.py
+++ erps21_reports/__manifest__.py
@@ -5,10 +5,11 @@
     'license': 'AGPL-3',
     'summary': 'Reporteria de ERP Siglo 21',
     'author': u'ERP Siglo 21',
-    'depends': ['stock',],
+    'depends': ['stock','l10n_cl_edi'],
     'data': [
         'reports/paper_format.xml',
         'reports/report_picking.xml',
+        'views/account_move_views'
     ],
     'installable': True,
     'auto_install': False,
